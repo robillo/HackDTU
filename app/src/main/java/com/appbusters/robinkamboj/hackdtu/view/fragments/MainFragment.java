b82fd7a201cfc1eca9a3f336636fadd86c2597a7
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-11 01:46:28 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/hackdtu/view/fragments/MainFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/hackdtu/view/fragments/MainFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.appbusters.robinkamboj.hackdtu.R;
 
@@ -14,17 +16,36 @@ import com.appbusters.robinkamboj.hackdtu.R;
  */
 public class MainFragment extends Fragment {
 
+    ImageView imageView1, imageView2;
+    Button private_walk, public_walk;
 
     public MainFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        imageView1 = (ImageView) v.findViewById(R.id.imageView1);
+        imageView2 = (ImageView) v.findViewById(R.id.imageView2);
+
+        private_walk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        public_walk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return v;
     }
 
 }

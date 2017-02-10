37d855a32ceaf7c6c9ef7ccf9cbc0483a9e2e814
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-11 01:21:21 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/hackdtu/view/MaineActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/hackdtu/view/MaineActivity.java
@@ -32,15 +32,6 @@ public class MaineActivity extends AppCompatActivity
         fragmentTransaction.replace(R.id.fragment_container, mainFragment);
         fragmentTransaction.commit();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);

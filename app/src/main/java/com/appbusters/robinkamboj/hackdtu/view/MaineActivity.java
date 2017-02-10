ea56b951f3e83d1f0649ee1682d60e81930d37f6
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-11 01:27:31 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/hackdtu/view/MaineActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/hackdtu/view/MaineActivity.java
@@ -84,10 +84,6 @@ public class MaineActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
